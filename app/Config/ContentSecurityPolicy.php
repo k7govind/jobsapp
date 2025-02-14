fc7d447c26c6996295b4efecb7c0117716fe2bcd
Govindarajan K <govindra@michaels.com>
2025-02-14 10:01:23 +0530

    Updated content security policies

--- app/Config/ContentSecurityPolicy.php
+++ app/Config/ContentSecurityPolicy.php
@@ -61,7 +61,7 @@ class ContentSecurityPolicy extends BaseConfig
      *
      * @var list<string>|string
      */
-    public $styleSrc = 'self';
+    public $styleSrc = ['self','https://fonts.googleapis.com'];
 
     /**
      * Defines the origins from which images can be loaded.
@@ -99,7 +99,7 @@ class ContentSecurityPolicy extends BaseConfig
      *
      * @var list<string>|string
      */
-    public $fontSrc;
+    public $fontSrc = ['self','https://fonts.gstatic.com'];
 
     /**
      * Lists valid endpoints for submission from `<form>` tags.
